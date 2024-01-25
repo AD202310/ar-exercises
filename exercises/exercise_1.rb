18d4874c8ba65153c2e2ffc4ae01a3cbeda42ebc
AD202310 <a.g.dychenko@gmail.com>
2024-01-24 22:27:47 -0500

    exercise_2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,18 +7,21 @@ puts "----------"
 
 # Create three stores
 Store.create(
+  id: 1,
   name: 'Burnaby', 
   annual_revenue: 300000, 
   mens_apparel: true, 
   womens_apparel: true)
 
 Store.create(
+  id: 2,
   name: 'Richmond', 
   annual_revenue: 1260000, 
   mens_apparel: false, 
   womens_apparel: true)
 
 Store.create(
+  id: 3,
   name: 'Gastown', 
   annual_revenue: 190000, 
   mens_apparel: true, 
@@ -26,3 +29,4 @@ Store.create(
 
 # Output the number of stores
 puts "Number of stores: #{Store.count}"
+puts "Store IDs: #{Store.first.id}"
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,13 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Load the first store (with id = 1) and assign it to @store1
+@store1 = Store.find(1)
+
+# Load the second store from the database and assign it to @store2
+@store2 = Store.find(2)
+
+# Update the name of the first store (@store1) in the database
+@store1.update(name: 'Metropolis')
+
+puts "Updated the name of the first store to: #{@store1.name}"
