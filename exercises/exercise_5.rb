15341681cf2410e827df2b1b2b6ca44d230ba3e2
AD202310 <a.g.dychenko@gmail.com>
2024-01-24 22:42:30 -0500

    exercise_5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,14 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Total revenue for the entire company (all stores)
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue for the Entire Company: #{total_revenue}"
+
+# Average annual revenue for all stores
+average_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue for All Stores: #{average_revenue}"
+
+# Number of stores generating $1M or more in annual sales
+stores_over_1m = Store.where("annual_revenue >= ?", 1000000).count
+puts "Number of Stores Generating $1M or More in Annual Sales: #{stores_over_1m}"
